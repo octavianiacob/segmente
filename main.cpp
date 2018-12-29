38de2fac79e869a4a10c751ddc05562fe0576998
anav238 <45420849+anav238@users.noreply.github.com>
2018-12-29 16:11:02 +0200

    added player vs computer game mode

--- main.cpp
+++ main.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
 #include <graphics.h>
 #include <winbgim.h>
+#include <math.h>
 #include <stdlib.h>
 #include <time.h>
 #include "algorithms.h"
 #include "customgraphics.h"
-#define MAX 100
+#define MAX_POINTS 100
+#define PLAYER1 RED
+#define PLAYER2 YELLOW
+Point points[MAX_POINTS];
 
-Point points[MAX];
+enum GameMode {
+    PvP,
+    PvC
+};
 
-int nPoints,nSegments,turn=RED;
-Segment seg[MAX];
+GameMode gameMode=PvC;
+int nPoints,nSegments;
+int turn=PLAYER1,player1Score,player2Score;
+Segment seg[MAX_POINTS/2];
 
-bool isOver() {
-    return false;
+bool isValidSegment(Segment s) {
+    if(s.a.x==s.b.x&&s.a.y==s.b.y)
+        return false;
+    for(int i=0;i<nSegments;i++)
+        if(doIntersect(s, seg[i]))
+            return false;
+    return true;
 }
 
-void addPoints(int number) {
+bool isGameOver() {
+    for(int i=0;i<nPoints;i++)
+    for(int j=0;j<nPoints;j++)
+        if(!isValidSegment({points[i], points[j]}))
+            return false;
+    return true;
+}
+
+void addPoints() {
     srand(time(NULL));
     int width=getwindowwidth();
     int height=getwindowheight();
-    for(int i=1;i<=number;i++) {
-        Point p={rand()%(width*4/5-10)+10,rand()%(height-10)+10};
+    for(int i=1;i<=8;i++)
+    for(int j=1;j<=10;j++) {
+        Point p={j*70-20,i*60-20};
+        p.x+=rand()%30-15;
+        p.y+=rand()%30-15;
         points[nPoints++]=p;
         drawDot(p);
     }
@@ -34,62 +59,114 @@ int getPointIndex(Point p) {
     return -1;
 }
 
-bool isValidSegment(Segment s) {
-    for(int i=0;i<nSegments;i++)
-        if(doIntersect(s, seg[i]))
-            return false;
-    return true;
-}
-
 void addSegmentToArray(int index1, int index2) {
     Segment s = {points[index1], points[index2]};
     seg[nSegments++]=s;
-    //swap(points[index1],points[nPoints-1]);
-    Point aux = points[index1];
-    points[index1]=points[nPoints-1];
-    points[nPoints-1]=aux;
-    //swap(points[index2],points[nPoints-2]);
-    aux = points[index2];
-    points[index2]=points[nPoints-2];
-    points[nPoints-2]=aux;
-    nPoints-=2;
+    points[index1].x=0;
+    points[index2].x=0;
 }
 
-bool isGameOver()
-{
+bool isBlocked(Point p) {
     for(int i=0;i<nPoints;i++)
-        for(int j=0;j<nPoints;j++)
-    {
-        if(!isValidSegment({points[i], points[j]}))
+        if(points[i].x&&isValidSegment({p,points[i]}))
             return false;
-    }
     return true;
+}
+
+bool isValidNeighbour(int i,int n) {
+    if(i+n<0||i+n>=nPoints)
+        return false;
+    if(points[i+n].x==0)
+        return false;
+    if(i%10==0&&(n==-11||n==-1||n==9))
+        return false;
+    if(i%10==9&&(n==-9||n==1||n==11))
+        return false;
+    return true;
+}
+
+int countFreeNeighbours(int pointIndex) {
+    int counter=0;
+    int neighbours[]={-11,-10,-9,-1,1,9,10,11};
+    for(int i=0;i<8;i++) {
+        int neighbourIndex=pointIndex+neighbours[i];
+        if(isValidNeighbour(pointIndex,neighbours[i]))
+            counter++;
+    }
+    return counter;
+}
+
+void doComputerMove() {
+    bool found=false;
+    int point1Index=-1,point2Index=-1;
+    int neighbours[]={-11,-10,-9,-1,1,9,10,11};
+    nSegments++;
+    for(int i=0;i<nPoints&&!found;i++) {
+        if(points[i].x&&countFreeNeighbours(i)<=4&&!isBlocked(points[i])) {
+            for(int j=0;j<8&&!found;j++)
+            for(int k=j+1;k<8&&!found;k++) {
+                int ji=i+neighbours[j],ki=i+neighbours[k];
+                if(isValidNeighbour(i,neighbours[j])&&isValidNeighbour(i,neighbours[k])
+                   &&isValidSegment({points[ji],points[ki]})) {
+                    seg[nSegments]={points[ji],points[ki]};
+                    int temp1=points[ji].x,temp2=points[ki].x;
+                    points[ji].x=points[ki].x=0;
+                    point1Index=ji,point2Index=ki;
+                    if(isBlocked(points[i])) {
+                        points[i].x=0;
+                        found=true;
+                    }
+                    points[ji].x=temp1;
+                    points[ki].x=temp2;
+                }
+            }
+        }
+    }
+    nSegments--;
+    if(point1Index==-1) {
+        for(int i=0;i<nPoints;i++)
+        for(int j=i+1;j<nPoints;j++)
+            if(points[i].x&&points[j].x&&isValidSegment({points[i],points[j]}))
+                point1Index=i,point2Index=j;
+    }
+    drawSegment(points[point1Index],points[point2Index],PLAYER2);
+    addSegmentToArray(point1Index,point2Index);
+}
+
+void doPlayerMove() {
+    int x,y,p1,p2;
+    do {
+        getmouseclick(WM_LBUTTONDOWN,x,y);
+        p1=getPointIndex({x,y});
+        if(p1<0)
+            continue;
+        do {
+            getmouseclick(WM_LBUTTONDOWN,x,y);
+            p2=getPointIndex({x,y});
+            if(p2>=0&&isValidSegment({points[p1],points[p2]})){
+                drawSegment(points[p1],points[p2],turn);
+                addSegmentToArray(p1,p2);
+            }
+            else
+                p2=-1;
+        } while(p2<0);
+    } while(p1<0);
 }
 
 void showGameScreen() {
     clearviewport();
     drawGameArea();
     drawScoreboard();
-    addPoints(50);
-    while(!isOver()) {
-        if(ismouseclick(WM_LBUTTONDOWN)) {
-            int x,y,p1,p2;
-            getmouseclick(WM_LBUTTONDOWN,x,y);
-            p1=getPointIndex({x,y});
-            if(p1>=0) {
-                do {
-                    getmouseclick(WM_LBUTTONDOWN,x,y);
-                    p2=getPointIndex({x,y});
-                    if(p2>=0&&isValidSegment({points[p1],points[p2]})){
-                        drawSegment(points[p1],points[p2],turn);
-                        addSegmentToArray(p1,p2);
-                    }
-                } while(p2<0);
-            }
-            if(turn==RED)
-                turn=YELLOW;
+    addPoints();
+    while(!isGameOver()) {
+        doPlayerMove();
+        if(gameMode==PvC&&!isGameOver())
+            doComputerMove();
+        else {
+            if(turn==PLAYER1)
+                turn=PLAYER2;
             else
-                turn=RED;
+                turn=PLAYER1;
         }
     }
 }
@@ -131,7 +208,6 @@ void startGame() {
 int main()
 {
     startGame();
-
     return 0;
 }
 
